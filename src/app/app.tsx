85030024ed2cf7e0300517f81392c4c16dc3a4f8
pilaf-pro <huynhnhattu5676@gmail.com>
2026-03-14 19:36:37 +0700

    feat: add toastify notification for error or success response message

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -2,11 +2,24 @@ import { AppProvider } from '@app/provider';
 import { router } from '@app/router';
 import type { JSX } from 'react';
 import { RouterProvider } from 'react-router';
+import { ToastContainer } from 'react-toastify';
 
 function App(): JSX.Element {
   return (
     <AppProvider>
       <RouterProvider router={router} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </AppProvider>
   );
 }
